04f47cbda86bc96363bea1f88d818c541ad33c18
Matthi Duarte <devscustomers@outlook.com>
2024-01-14 03:57:15 -0500

    operadores

---
+++ Unidad04/leccion_03_operadores_comparacion.js
@@ -0,0 +1,32 @@
+// Operadores de comparación:
+
+let x = 5;
+let y = '5';
+
+// Operador de igualdad:
+console.log(x == y); // Salida: true (después de la coerción de tipos)
+
+// Operador de igualdad estricta:
+console.log(x === y); // Salida: false
+
+// Operador de no igualdad:
+console.log(x != y); // Salida: false (después de la coerción de tipos)
+
+// Operador de no igualdad estricta:
+console.log(x !== y); // Salida: true
+
+let a = 10;
+let b = 5;
+let c = 5;
+
+// Operador "menor que":
+console.log(a < b); // Salida: false
+
+// Operador "menor o igual que":
+console.log(c <= b); // Salida: true
+
+// Operador "mayor que":
+console.log(a > b); // Salida: true
+
+// Operador "mayor o igual que":
+console.log(a >= b); // Salida: true

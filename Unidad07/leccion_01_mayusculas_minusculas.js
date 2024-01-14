b045826d12d5c5ee83069df7b4372a5dcf980841
Matthi Duarte <devscustomers@outlook.com>
2024-01-14 03:58:51 -0500

    concatenacion, arreglos, expresiones-regulares

---
+++ Unidad07/leccion_01_mayusculas_minusculas.js
@@ -0,0 +1,11 @@
+// La clase String contiene varios métodos que utilizamos para
+// manipular strings
+
+const textoEnMinuscula = `Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit`;
+
+const textoEnMayuscula = textoEnMinuscula.toUpperCase();
+
+console.log(textoEnMayuscula);
+
+console.log(textoEnMayuscula.toLowerCase());

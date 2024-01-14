ba8b09f97d2614afc73f9633b7789fb8bd24d76d
Matthi Duarte <devscustomers@outlook.com>
2024-01-14 02:31:29 -0500

    Delete Unidad01 directory

--- Unidad01/leccion_06_comentarios.js
+++
@@ -1,9 +0,0 @@
-// Un comentario de una sola línea
-console.log("Hola mundo!");
-
-/* Otro comentario de una sola línea */
-console.log("Aprendamos JavaScript!");
-
-/* Un comentario de 
-varias líneas */
-console.log("Programar es divertido!");
